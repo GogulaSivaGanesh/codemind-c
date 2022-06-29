dc7f79171b7b63cfb5fdaf3d8715a624584e1f15
GogulaSivaGanesh <97424280+GogulaSivaGanesh@users.noreply.github.com>
2022-06-29 21:31:20 +0530

    #include<stdio.h> int main() {     int n,i,j,k,z=2;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         for(j=n-1;j>=i;j--)         {             printf(" ");         }         for(k=1;k<z;k++)         {             printf("%d",i);         }         z+=2;         printf(" ");     }     return 0; }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -1,22 +1,21 @@
 #include<stdio.h>
 int main()
 {
-    int n,i,j;
+    int n,i,j,k,z=2;
     scanf("%d",&n);
     for(i=1;i<=n;i++)
     {
-        for(j=1;j<=n;j++)
+        for(j=n-1;j>=i;j--)
         {
-            if(j==i || i+j==(n+1))
-            {
-                printf("x");
-            }
-            else
-            {
-                printf("0");
-            }
+            printf(" ");
         }
+        for(k=1;k<z;k++)
+        {
+            printf("%d",i);
+        }
+        z+=2;
         printf("
 ");
     }
+    return 0;
 }
